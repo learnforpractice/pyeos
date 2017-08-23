sionnow0/eos

Implement floating point API

contracts/test_api/test_math.cpp
@@ -28,3 +28,47 @@ unsigned int test_math::test_diveq_i128_by_0() {
   diveq_i128(&a, &b);
   return WASM_TEST_PASS;
 }
+
+unsigned int test_math::test_double_api() {
+  
+  uint64_t res = double_mult( 
+    double_div( i64_to_double(2), i64_to_double(7) ),
+    double_add( i64_to_double(3), i64_to_double(2) )
+  );
+  
+  WASM_ASSERT( double_to_i64(res) == 1, "double funcs");
+
+  res = double_eq(
+    double_div( i64_to_double(5), i64_to_double(9) ),
+    double_div( i64_to_double(10), i64_to_double(18) )
+  );
+
+  WASM_ASSERT(res == 1, "double_eq");
+
+  res = double_gt(
+    double_div( i64_to_double(9999999), i64_to_double(7777777) ),
+    double_div( i64_to_double(9999998), i64_to_double(7777777) )
+  );
+
+  WASM_ASSERT(res == 1, "double_gt");
+
+  res = double_lt(
+    double_div( i64_to_double(9999998), i64_to_double(7777777) ),
+    double_div( i64_to_double(9999999), i64_to_double(7777777) )
+  );
+
+  WASM_ASSERT(res == 1, "double_lt");
+  
+  return WASM_TEST_PASS;
+}
+
+unsigned int test_math::test_double_api_div0() {
+  
+  double_div( i64_to_double(1), 
+    double_add( 
+      i64_to_double(-5), i64_to_double(5) 
+    )
+  );
+
+  return WASM_TEST_PASS;
+}

learnforpractice/pyeos

contracts/test_api/test_math.cpp
@@ -28,3 +28,47 @@ unsigned int test_math::test_diveq_i128_by_0() {
   diveq_i128(&a, &b);
   return WASM_TEST_PASS;
 }
+
+unsigned int test_math::test_double_api() {
+  
+  uint64_t res = double_mult( 
+    double_div( i64_to_double(2), i64_to_double(7) ),
+    double_add( i64_to_double(3), i64_to_double(2) )
+  );
+  
+  WASM_ASSERT( double_to_i64(res) == 1, "double funcs");
+
+  res = double_eq(
+    double_div( i64_to_double(5), i64_to_double(9) ),
+    double_div( i64_to_double(10), i64_to_double(18) )
+  );
+
+  WASM_ASSERT(res == 1, "double_eq");
+
+  res = double_gt(
+    double_div( i64_to_double(9999999), i64_to_double(7777777) ),
+    double_div( i64_to_double(9999998), i64_to_double(7777777) )
+  );
+
+  WASM_ASSERT(res == 1, "double_gt");
+
+  res = double_lt(
+    double_div( i64_to_double(9999998), i64_to_double(7777777) ),
+    double_div( i64_to_double(9999999), i64_to_double(7777777) )
+  );
+
+  WASM_ASSERT(res == 1, "double_lt");
+  
+  return WASM_TEST_PASS;
+}
+
+unsigned int test_math::test_double_api_div_0() {
+  
+  double_div( i64_to_double(1), 
+    double_add( 
+      i64_to_double(-5), i64_to_double(5) 
+    )
+  );
+
+  return WASM_TEST_PASS;
+}