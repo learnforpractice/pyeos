2ece09d7efe06113facb2d8ef883830c82c25e16
learnforpractice <learnforpractice@gmail.com>
2017-09-08 13:14:47 +0800

    auto convert pyx to cpp

--- programs/pyeos/wrap.sh
+++
@@ -1,4 +0,0 @@
-cython --cplus eosapi.pyx -o eosapi_wrap.cpp
-#cython --cplus eostypes_.pyx -o eostypes_wrap.cpp
-cython --cplus wallet.pyx -o wallet_wrap.cpp
-cython --cplus eostypes_.pyx -o eostypes_wrap.cpp
