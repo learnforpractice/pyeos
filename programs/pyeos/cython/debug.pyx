3a9b2d42b9a5e8d1fce2e3e36d0b0a7b31cd5809
learnforpractice <learnforpractice@gmail.com>
2017-12-19 09:17:59 +0800

    add some debug function

--- programs/pyeos/cython/debug.pyx
+++ programs/pyeos/cython/debug.pyx
@@ -1,6 +1,10 @@
 cdef extern from "<eos/chain/python_interface.hpp>":
     void tinypy_run_code_(const char* code)
 
+    void Py_EnableCodeExecution(int enable, int _only_once)
+    void Py_SetWhiteList(const char** _white_list);
+    void Py_EnableImportWhiteList(int enable);
+
 def tinypy_run_code(code):
     if isinstance(code, str):
         code = code.encode('utf8')
@@ -17,3 +21,13 @@ def show_debug_contracts():
 
 def stop_at_load_module(stop):
     tinypy_run_code("import python_contract;python_contract.stop_at_load_module('{0}')".format(stop))
+
+def enable_code_execution(enable, only_once):
+    Py_EnableCodeExecution(enable, only_once)
+
+def enable_whitelist(enable):
+    if enable:
+        Py_EnableImportWhiteList(1)
+    else:
+        Py_EnableImportWhiteList(0)
+
