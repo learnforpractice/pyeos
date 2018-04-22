2570f9335c5bb5f16a04e8bbfebf67ce49569a5c
learnforpractice <learnforpractice@gmail.com>
2018-04-22 16:13:37 +0800

    move Sync to common

--- programs/pyeos/contracts/common/__init__.py
+++ programs/pyeos/contracts/common/__init__.py
@@ -1,7 +1,9 @@
 import os
+import pickle
+
 import initeos
 import eosapi
-import pickle
+import database_api
 
 producer = eosapi.Producer()
 CODE_TYPE_WAST = 0
@@ -46,4 +48,122 @@ def init_(name, src, abi, curr_path, code_type=CODE_TYPE_MPY):
         else:
             r = eosapi.set_contract(name, src, abi, 1)
         assert r
+
+class Sync(object):
+    def __init__(self, _account, _dir = None, _ignore = []):
+        self.ignore_files = _ignore 
+
+        if not isinstance(_account, int):
+            self.account = eosapi.N(_account)
+        else:
+            self.account = _account
+
+        if _dir:
+            if not os.path.exists(_dir):
+                raise Exception(_dir + ' not found')
+            self.src_dir = _dir
+        else:
+            self.src_dir = os.path.dirname(os.path.abspath(__file__))
+
+    def need_update(self, src_dir, src_file, mpy=False):
+        id = eosapi.hash64(src_file)
+        code = self.account
+        itr = database_api.find_i64(code, code, code, id)
+        if itr < 0:
+            return True
+
+        old_src = database_api.get_i64(itr)
+        _full_file_path = os.path.join(src_dir, src_file)
+
+        if not mpy:
+            with open(_full_file_path, 'rb') as f:
+                if old_src[1:] == f.read():
+                    return False
+        else:
+            src_code = eosapi.mp_compile(_full_file_path)
+            if old_src[1:] == src_code:
+                return False
+        return True
+
+    def deploy(self, src_file, _dir = None):
+        _abs_file_path = None
+        if not _dir:
+            _dir = self.src_dir
+
+        _abs_file_path = os.path.join(_dir, src_file)
+
+        if not self.need_update(_dir, src_file):
+            print('++++++++++:', src_file, "up to date")
+            return
+
+        src_code = open(_abs_file_path, 'rb').read()
+        mod_name = src_file #os.path.basename(src_file)
+        msg = int.to_bytes(len(mod_name), 1, 'little')
+        msg += mod_name.encode('utf8')
+        msg += int.to_bytes(0, 1, 'little') # source code
+        msg += src_code
+        with producer:
+            print('++++++++++++++++deply:', mod_name)
+            r = eosapi.push_message('kitties','deploy',msg,{'kitties':'active'},rawargs=True)
+            assert r
+
+    def deploy_depend_libs(self, _libs_dir, depend_libs):
+        for file_name in depend_libs:
+            _src_code_path = os.path.join(_libs_dir, file_name)
+            if not self.need_update(_libs_dir, file_name):
+                continue
+            src_code = open(_src_code_path, 'r').read()
+            mod_name = file_name
+            msg = int.to_bytes(len(mod_name), 1, 'little')
+            msg += mod_name.encode('utf8')
+            msg += int.to_bytes(0, 1, 'little') # source code
+            msg += src_code.encode('utf8')
+
+            print('++++++++++++++++deply:', file_name)
+            r = eosapi.push_message('kitties','deploy',msg,{'kitties':'active'},rawargs=True)
+            assert r
+        producer.produce_block()
+
+    def deploy_all(self):
+        files = os.listdir(self.src_dir)
+        for file_name in files:
+            _abs_file_path = os.path.join(self.src_dir, file_name)
+            if not file_name.endswith('.py'):
+                continue
+            if file_name in self.ignore_files:
+                continue
+            self.deploy(file_name)
+#('main.py', 'test.py', 'ustruct.py', 'eoslib.py')
+
+    def deploy_mpy(self, file_name):
+        _abs_file_path = os.path.join(self.src_dir, file_name)
+        if not self.need_update(self.src_dir, _abs_file_path, mpy=True):
+            print('++++++++++:', file_name, "up to date")
+            return
+
+        src_code = eosapi.mp_compile(_abs_file_path)
+        file_name = file_name.replace('.py', '.mpy')
+        mod_name = file_name
+        msg = int.to_bytes(len(mod_name), 1, 'little')
+        msg += mod_name.encode('utf8')
+        msg += int.to_bytes(1, 1, 'little') # compiled code
+        msg += src_code
+
+        print('++++++++++++++++deply:', file_name)
+        r = eosapi.push_message('kitties','deploy',msg,{'kitties':'active'},rawargs=True)
+        assert r
+
+        producer.produce_block()
+        self.save()
+        time.sleep(0.2)
+
+    def deploy_all_mpy(self):
+        with producer:
+            files = os.listdir(self.src_dir)
+            for file_name in files:
+                if not file_name.endswith('.py'):
+                    continue
+                if file_name in self.ignore_files:
+                    continue
+                self.deploy_mpy(file_name)
 
--- programs/pyeos/contracts/cryptokitties/test.py
+++ programs/pyeos/contracts/cryptokitties/test.py
@@ -6,146 +6,25 @@ import initeos
 import subprocess
 import pickle
 
-from common import init_, producer
+from common import init_, producer, Sync
 
 print('please make sure you are running the following command before test')
 print('./pyeos/pyeos --manual-gen-block --debug -i')
 
 def init(func):
     def func_wrapper(*args):
-        init_('kitties', 'main.py', 'cryptokitties.abi', __file__, mpy=True)
+        init_('kitties', 'main.py', 'cryptokitties.abi', __file__, 2)
         return func(*args)
     return func_wrapper
 
-class Sync(object):
-    def __init__(self, _dir = None, _ignore = []):
-        self.last_sync = {}
-        self.ignore_files = _ignore 
-        if _dir:
-            if not os.path.exists(_dir):
-                raise Exception(_dir + ' not found')
-            self.src_dir = _dir
-        else:
-            self.src_dir = os.path.dirname(os.path.abspath(__file__))
+_dir = os.path.dirname(os.path.abspath(__file__))
 
-        self._sync_file = os.path.join(self.src_dir, '.last_sync')
-
-        try:
-            with open(self._sync_file, 'rb') as f:
-                self.last_sync = pickle.load(f)
-        except:
-            pass
-
-    def check(self, src_code):
-        if not src_code in self.last_sync:
-            self.last_sync[src_code] = os.path.getmtime(src_code)
-            return False
-        if self.last_sync[src_code] == os.path.getmtime(src_code):
-            return True
-        else:
-            self.last_sync[src_code] = os.path.getmtime(src_code)
-            return False
-
-    def save(self):
-        with open(self._sync_file, 'wb') as f:
-            pickle.dump(self.last_sync, f)
-
-    def deploy(self, src_file, _dir = None):
-        _abs_file_path = None
-        if _dir:
-            _abs_file_path = os.path.join(_dir, src_file)
-        else:
-            _abs_file_path = os.path.join(self.src_dir, src_file)
-
-        if self.check(_abs_file_path):
-            print('++++++++++:', src_file, "up to date")
-            return
-
-        src_code = open(_abs_file_path, 'rb').read()
-        mod_name = src_file #os.path.basename(src_file)
-        msg = int.to_bytes(len(mod_name), 1, 'little')
-        msg += mod_name.encode('utf8')
-        msg += int.to_bytes(0, 1, 'little') # source code
-        msg += src_code
-        with producer:
-            print('++++++++++++++++deply:', mod_name)
-            r = eosapi.push_message('kitties','deploy',msg,{'kitties':'active'},rawargs=True)
-            assert r
-        self.save()
-
-    def deploy_depend_libs(self):
-        depend_libs = ['cache.py', 'storage.py']
-        _libs_dir = os.path.join(self.src_dir, ".." ,'libs')
-        for file_name in depend_libs:
-            _src_code_path = os.path.join(_libs_dir, file_name)
-            if self.check(_src_code_path):
-                continue
-            src_code = open(_src_code_path, 'r').read()
-            mod_name = file_name
-            msg = int.to_bytes(len(mod_name), 1, 'little')
-            msg += mod_name.encode('utf8')
-            msg += int.to_bytes(0, 1, 'little') # source code
-            msg += src_code.encode('utf8')
-
-            print('++++++++++++++++deply:', file_name)
-            r = eosapi.push_message('kitties','deploy',msg,{'kitties':'active'},rawargs=True)
-            assert r
-        producer.produce_block()
-
-    def deploy_all(self):
-        self.deploy_depend_libs()
-        files = os.listdir(self.src_dir)
-        for file_name in files:
-            _abs_file_path = os.path.join(self.src_dir, file_name)
-            if not file_name.endswith('.py'):
-                continue
-            if file_name in self.ignore_files:
-                continue
-            self.deploy(file_name)
-#('main.py', 'test.py', 'ustruct.py', 'eoslib.py')
-
-    def deploy_mpy(self, file_name):
-        _abs_file_path = os.path.join(self.src_dir, file_name)
-        if self.check(_abs_file_path):
-            print('++++++++++:', file_name, "up to date")
-            return
-
-        src_code = eosapi.mp_compile(_abs_file_path)
-        file_name = file_name.replace('.py', '.mpy')
-        mod_name = file_name
-        msg = int.to_bytes(len(mod_name), 1, 'little')
-        msg += mod_name.encode('utf8')
-        msg += int.to_bytes(1, 1, 'little') # compiled code
-        msg += src_code
-
-        print('++++++++++++++++deply:', file_name)
-        r = eosapi.push_message('kitties','deploy',msg,{'kitties':'active'},rawargs=True)
-        assert r
-
-        producer.produce_block()
-        self.save()
-        time.sleep(0.2)
-
-    def deploy_all_mpy(self):
-        self.deploy_depend_libs()
-        with producer:
-            files = os.listdir(self.src_dir)
-            for file_name in files:
-                if not file_name.endswith('.py'):
-                    continue
-                if file_name in self.ignore_files:
-                    continue
-                self.deploy_mpy(file_name)
-    def clean(self):
-        self.last_sync = {}
-        self.save()
-        print('clean done')
-
-sync = Sync(_ignore = ('main.py', 'test.py', 'ustruct.py', 'eoslib.py'))
+sync = Sync(_account = 'kitties', _dir = _dir, _ignore = ('main.py', 'test.py', 'ustruct.py', 'eoslib.py'))
 
 @init
 def deploy_depend_libs():
-    sync.deploy_depend_libs()
+    from backyard import test
+    test.deploy_mpy()
 
 @init
 def deploy_all():
