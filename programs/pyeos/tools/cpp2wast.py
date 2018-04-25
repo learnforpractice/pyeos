dc13bdfcff34eb9262e414c3908c9a5738d02b4f
learnforpractice <learnforpractice@gmail.com>
2018-04-25 18:54:11 +0800

    Tools for generating, building and testing code

--- programs/pyeos/tools/cpp2wast.py
+++ programs/pyeos/tools/cpp2wast.py
@@ -1,17 +1,48 @@
-#./pyeos/pyeos --manual_gen_block --debug -i
 import os
+import sys
 import time
+import shlex
 import struct
+import subprocess
+from optparse import OptionParser
 
 import wallet
 import eosapi
+import initeos
 from eosapi import N
 
-import initeos
-import subprocess
-import  shlex
 
-src_path = os.path.dirname(os.path.abspath(__file__))
+#src_path = os.path.dirname(os.path.abspath(__file__))
+src_path = '../../programs/pyeos/contracts/lab'
+
+src_root_path = '/Users/newworld/dev/pyeos'
+boost_path = '/Users/newworld/.hunter/_Base/a882971/f6bfdc3/786a69d/Install/include'
+
+clang = '/usr/local/wasm/bin/clang'
+link = '/usr/local/wasm/bin/llvm-link'
+
+llc = '/usr/local/wasm/bin/llc'
+s2wast = '/Users/newworld/dev/pyeos/build/externals/binaryen/bin/eosio-s2wasm'
+
+parser = OptionParser()
+parser.add_option("--clang",            dest="clang",           default=clang,          help="")
+parser.add_option("--link",             dest="link",            default=link,           help="")
+parser.add_option("--llc",              dest="llc",             default=llc,            help="")
+parser.add_option("--s2wast",           dest="s2wast",          default=s2wast,         help="")
+parser.add_option("--src-path",         dest="src_path",        default=src_path,       help="Folder to locate source code to compile")
+parser.add_option("--src-root-path",    dest="src_root_path",   default=src_root_path,  help="Eos source code root path")
+parser.add_option("--boost-path",       dest="boost_path",      default=boost_path,     help="Boost installed path")
+
+
+(options, args) = parser.parse_args()
+
+clang           = options.clang
+link            = options.link
+llc             = options.llc
+s2wast          = options.s2wast
+src_path        = options.src_path
+src_root_path   = options.src_root_path
+boost_path      = options.boost_path
 
 def set_src_path(_path):
     global src_path
@@ -23,13 +54,16 @@ def need_build(src_file):
     wast_file = src_file.replace('.cpp', '.wast')
     full_wast_file_path = os.path.join(src_path, wast_file)
 
+    if not os.path.exists(full_wast_file_path):
+        return True
+
     t1 = os.path.getmtime(full_src_file_path)
     t2 = os.path.getmtime(full_wast_file_path)
     if t1 > t2:
         return True
     return False
 
-def build(src_file = 'code.cpp', force=False):
+def build(src_file = 'lab.cpp', force=False):
     if not force:
         if not need_build(src_file):
             return True
@@ -39,10 +73,10 @@ def build(src_file = 'code.cpp', force=False):
     full_src_file_path = os.path.join(src_path, src_file)
     full_cpp_bc_file_path = os.path.join(src_path, cpp_bc_file)
 
-    clang_cmd = "/usr/local/wasm/bin/clang;-emit-llvm;-O3;--std=c++14;--target=wasm32;-ffreestanding;-nostdlib;-nostdlibinc;-fno-threadsafe-statics;-fno-rtti;-fno-exceptions;-c;{0};-o;{1};-Weverything;-Wno-c++98-compat;-Wno-old-style-cast;-Wno-vla;-Wno-vla-extension;-Wno-c++98-compat-pedantic;-Wno-missing-prototypes;-Wno-missing-variable-declarations;-Wno-packed;-Wno-padded;-Wno-c99-extensions;-Wno-documentation-unknown-command;-I;/Users/newworld/dev/pyeos/contracts;-I;/Users/newworld/dev/pyeos/externals/magic_get/include;-isystem;/Users/newworld/dev/pyeos/contracts/libc++/upstream/include;-isystem;/Users/newworld/dev/pyeos/contracts/musl/upstream/include;-isystem;/Users/newworld/.hunter/_Base/a882971/f6bfdc3/786a69d/Install/include;-isystem;/Users/newworld/dev/pyeos/contracts/libc++/upstream/include;-isystem;/Users/newworld/dev/pyeos/contracts/musl/upstream/include;-isystem;/Users/newworld/.hunter/_Base/a882971/f6bfdc3/786a69d/Install/include"
+    clang_cmd = "{clang};-emit-llvm;-O3;--std=c++14;--target=wasm32;-ffreestanding;-nostdlib;-nostdlibinc;-fno-threadsafe-statics;-fno-rtti;-fno-exceptions;-c;{0};-o;{1};-Weverything;-Wno-c++98-compat;-Wno-old-style-cast;-Wno-vla;-Wno-vla-extension;-Wno-c++98-compat-pedantic;-Wno-missing-prototypes;-Wno-missing-variable-declarations;-Wno-packed;-Wno-padded;-Wno-c99-extensions;-Wno-documentation-unknown-command;-I;{2}/contracts;-I;{2}/externals/magic_get/include;-isystem;{2}/contracts/libc++/upstream/include;-isystem;{2}/contracts/musl/upstream/include;-isystem;{2}/contracts/libc++/upstream/include;-isystem;{2}/contracts/musl/upstream/include;-isystem;{boost_path}"
     clang_cmd = clang_cmd.replace(';', ' ')
-    cmds = shlex.split(clang_cmd.format(full_src_file_path, full_cpp_bc_file_path))
-#    print(cmds)
+    cmds = shlex.split(clang_cmd.format(full_src_file_path, full_cpp_bc_file_path, src_root_path, clang=clang, boost_path=boost_path))
+    print(cmds)
     ret = subprocess.call(cmds)
     print('compile ', ret)
     if ret:
@@ -52,9 +86,10 @@ def build(src_file = 'code.cpp', force=False):
     bc_file = src_file.replace('.cpp', '.bc')
     full_bc_file_path = os.path.join(src_path, bc_file)
 
-    link_cmd = '/usr/local/wasm/bin/llvm-link -only-needed -o {0} {1} /Users/newworld/dev/pyeos/build/contracts/libc++/libc++.bc;/Users/newworld/dev/pyeos/build/contracts/musl/libc.bc;/Users/newworld/dev/pyeos/build/contracts/eosiolib/eosiolib.bc'
+    link_cmd = '{link} -only-needed -o {0} {1} {2}/build/contracts/libc++/libc++.bc;{2}/build/contracts/musl/libc.bc;{2}/build/contracts/eosiolib/eosiolib.bc'
     link_cmd = link_cmd.replace(';', ' ')
-    cmds = shlex.split(link_cmd.format(full_bc_file_path, full_cpp_bc_file_path))
+    cmds = shlex.split(link_cmd.format(full_bc_file_path, full_cpp_bc_file_path, src_root_path, link=link))
+    print(cmds)
     ret = subprocess.call(cmds)
     print('link', ret)
     if ret:
@@ -63,8 +98,9 @@ def build(src_file = 'code.cpp', force=False):
     s_file = src_file.replace('.cpp', '.s')
     full_s_file_path = os.path.join(src_path, s_file)
 
-    llc_cmd = "/usr/local/wasm/bin/llc -thread-model=single -asm-verbose=false -o {0} {1}".format(full_s_file_path, full_bc_file_path)
+    llc_cmd = "{llc} -thread-model=single -asm-verbose=false -o {0} {1}".format(full_s_file_path, full_bc_file_path, llc=llc)
     cmds = shlex.split(llc_cmd)
+    print(cmds)
     ret = subprocess.call(cmds)
     print('llc', ret)
     if ret:
@@ -73,8 +109,10 @@ def build(src_file = 'code.cpp', force=False):
     wast_file = src_file.replace('.cpp', '.wast')
     full_wast_file_path = os.path.join(src_path, wast_file)
 
-    s2wasm_cmd = '/Users/newworld/dev/pyeos/build/externals/binaryen/bin/eosio-s2wasm -o {0} -s 4096  {1}'.format(full_wast_file_path, full_s_file_path)
+    s2wasm_cmd = '{s2wast} -o {0} -s 4096  {1}'.format(full_wast_file_path, full_s_file_path,s2wast=s2wast)
     cmds = shlex.split(s2wasm_cmd)
+    print(cmds)
+
     ret = subprocess.call(cmds)
     print('s2wasm', ret)
     if ret:
---
+++ programs/pyeos/tools/sketch.py
@@ -0,0 +1,132 @@
+import os
+import sys
+from optparse import OptionParser
+
+test_py = \
+'''import os
+import time
+
+import wallet
+import eosapi
+import initeos
+from tools import cpp2wast
+
+from common import init_, producer
+
+print('please make sure you are running the following command before test')
+print('./pyeos/pyeos --manual-gen-block --debug -i')
+
+def init(func):
+    def func_wrapper(wasm={1}, *args, **kw_args):
+        if wasm:
+            src_path = os.path.dirname(os.path.abspath(__file__))
+            cpp2wast.set_src_path(src_path)
+            cpp2wast.build('{0}.cpp')
+            init_('{0}', '{0}.wast', '{0}.abi', __file__, 0)
+        else:
+            init_('{0}', '{0}.py', '{0}.abi', __file__, 2)
+        return func(*args, **kw_args)
+    return func_wrapper
+
+@init
+def test(msg='hello,world', wasm={1}):
+    with producer:
+        r = eosapi.push_message('{0}', 'sayhello', msg, {{'{0}':'active'}}, rawargs=True)
+        assert r
+'''
+py_src = \
+'''from eoslib import *
+def apply(name, type):
+    if type == N('sayhello'):
+        require_auth(N('{0}'))
+        msg = read_action()
+        print(msg.decode('utf8'))
+'''
+
+cpp_src = \
+'''#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+extern "C" {{
+   void apply( uint64_t receiver, uint64_t code, uint64_t action ) {{
+      auto self = receiver;
+      if( code == self ) {{
+         switch( action ) {{
+            case N(sayhello):
+                  size_t size = action_data_size();
+                  char msg[size+1];
+                  msg[size] = '\\0';
+                  read_action_data(msg, size);
+                  prints(msg);
+            break;
+         }}
+         eosio_exit(0);
+      }}
+   }}
+}}
+'''
+
+abi_src = \
+'''{
+  "actions": [{
+      "name": "sayhello",
+      "type": "raw"
+    }
+  ]
+}
+'''
+
+class Sketch(object):
+    def __init__(self, _account, _project_dir = '.', _project_type='py'):
+        self.account = _account
+        self.project_dir = _project_dir
+        self.project_type = _project_type
+
+        self.mkdir(_project_dir)
+
+    def build(self):
+        self.build_source()
+        self.build_abi()
+        self.build_test()
+
+    def build_source(self):
+        if self.project_type == 'py':
+            with open(os.path.join(self.project_dir, self.account+'.py'), 'w') as f:
+                f.write(py_src.format(self.account))
+        elif self.project_type == 'cpp':
+            with open(os.path.join(self.project_dir, self.account+'.cpp'), 'w') as f:
+                f.write(cpp_src.format(self.account))
+        else:
+            raise Exception('language not supported: '+self.project_type)
+
+    def build_abi(self):
+        with open(os.path.join(self.project_dir, self.account+'.abi'), 'w') as f:
+            f.write(abi_src)
+
+    def build_test(self):
+        wasm = False
+        if self.project_type == 'cpp':
+            wasm = True
+        with open(os.path.join(self.project_dir, 'test.py'), 'w') as f:
+            f.write(test_py.format(self.account, wasm))
+
+    def mkdir(self, _path):
+        _path = os.path.normpath(_path)
+        __path = ''
+        for _cur_dir in _path.split(os.sep):
+            __path = os.path.join(__path, _cur_dir)
+            if not os.path.exists(__path):
+                os.mkdir(__path)
+  
+if __name__ == '__main__':
+    usage = "usage: python sketch.py [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("--account",  dest="account", default='world',   help="Account name")
+    parser.add_option("--lang",     dest="lang",    default='py',   help="Language type, 'py' or 'cpp'")
+    parser.add_option("--dir",      dest="dir",     default='helloworld',    help="Project directory")
+    parser.add_option("--init",     dest="init",    default=True,   action="store_true", help="Initialize project")
+
+    (options, args) = parser.parse_args()
+    print(options, args)
+    s = Sketch(options.account, options.dir, options.lang)
+    s.build()
+
