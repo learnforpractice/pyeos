5833a56f9f8ceaaa61c918a9ca99eb7fd3409727
learnforpractice <learnforpractice@gmail.com>
2018-04-29 20:40:18 +0800

    Fix for name convertion and buffer overflow

--- programs/pyeos/contracts/lab/lab.cpp
+++ programs/pyeos/contracts/lab/lab.cpp
@@ -7,16 +7,18 @@ extern "C" {
    }
    void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
       auto self = receiver;
-      prints("hello,world\n");
       if( code == self ) {
          switch( action ) {
-            case N('sayhello'):
-                size_t size = action_data_size();
-                char msg[size+2];
-                msg[size] = '\n';
-                msg[size+1] = '\0';
-                read_action_data(msg, size);
-                prints(msg);
+            case N(sayhello):
+                  size_t size = action_data_size();
+                  if (size > 128) {
+                     size = 128;
+                  }
+                  char msg[size+2];
+                  msg[size] = '\n';
+                  msg[size+1] = '\0';
+                  read_action_data(msg, size);
+                  prints(msg);
             break;
          }
          eosio_exit(0);
