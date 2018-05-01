ddd708192ef86bf9f20af26d774cdb90c3a0c1af
learnforpractice <learnforpractice@gmail.com>
2018-05-01 21:00:35 +0800

    testcase for memory leak in micropython during updating source code, use m_del_obj to delete the replaced module

--- programs/pyeos/contracts/bugs/test.py
+++ programs/pyeos/contracts/bugs/test.py
@@ -1,7 +1,11 @@
+import os
 import time
+import struct
 import wallet
 import eosapi
 import initeos
+
+from eosapi import N
 
 from common import smart_call, producer
 
@@ -25,3 +29,15 @@ def t2():
         assert r
     eosapi.produce_block()
 
+@init
+def test_module_memory_leak(sign=True):
+    _src_dir = os.path.dirname(__file__)
+    actions = []
+    for i in range(10000):
+        code = struct.pack('QBB', N('bugs'), 1, 0)
+        with open(os.path.join(_src_dir, 'bugs.py'), 'rb') as f:
+            code += eosapi.pack_bytes(b'\x00'+f.read() + b'\n1==(%d)'%(i+100,))
+        act = [N('eosio'), N('setcode'), [[N('bugs'), N('active')]], code]
+        cost_time = eosapi.push_transactions2([[act]], sign)
+        if i % 50 == 0:
+            eosapi.produce_block()
