7900a909239697a1b293180d2770a7de9b057101
learnforpractice <learnforpractice@gmail.com>
2018-06-08 21:46:02 +0800

    more readable test code

--- programs/pyeos/contracts/hello/hello.py
+++ programs/pyeos/contracts/hello/hello.py
@@ -6,14 +6,17 @@ def sayHello():
 
     name = read_action()
     print('hello', name)
-
-    itr = db_find_i64(n, n, n, id)
+    code = n
+    scope = n
+    table = n
+    payer = n
+    itr = db_find_i64(code, scope, table, id)
     if itr >= 0: # value exist, update it
         old_name = db_get_i64(itr)
         print('hello,', old_name)
-        db_update_i64(itr, n, name)
+        db_update_i64(itr, payer, name)
     else:
-        db_store_i64(n, n, n, id, name)
+        db_store_i64(scope, table, payer, id, name)
 
 def play():
     from backyard import garden
