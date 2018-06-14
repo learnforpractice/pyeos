49c2f91d30d52721d2b437980c3024fe784834eb
learnforpractice <learnforpractice@gmail.com>
2018-06-14 15:39:54 +0800

    Update sample code

--- programs/pyeos/contracts/native/native.py
+++ programs/pyeos/contracts/native/native.py
@@ -1,12 +1,13 @@
 from eoslib import *
+import db
 def deploy(account, src_code):
     code = N('native')
     print('++++++++++++deploy:account', account)
-    itr = db_find_i64(code, code, code, account)
+    itr = db.find_i64(code, code, code, account)
     if itr < 0:
-        db_store_i64(code, code, code, account, src_code)
+        db.store_i64(code, code, code, account, src_code)
     else:
-        db_update_i64(itr, code, src_code)
+        db.update_i64(itr, code, src_code)
 
 def apply(receiver, code, action):
     print(n2s(code), n2s(action))
--- programs/pyeos/contracts/vmstore/vmstore.py
+++ programs/pyeos/contracts/vmstore/vmstore.py
@@ -1,12 +1,14 @@
+import db
 from eoslib import *
-code = N('vmstore')
+
 def deploy(vm_name, src_code):
+    code = N('vmstore')
     print('++++++++++++deploy:vm_name', vm_name)
-    itr = db_find_i64(code, code, code, vm_name)
+    itr = db.find_i64(code, code, code, vm_name)
     if itr < 0:
-        db_store_i64(code, code, code, vm_name, src_code)
+        db.store_i64(code, code, code, vm_name, src_code)
     else:
-        db_update_i64(itr, code, src_code)
+        db.update_i64(itr, code, src_code)
 
 def apply(receiver, code, action):
     print(n2s(code), n2s(action))
