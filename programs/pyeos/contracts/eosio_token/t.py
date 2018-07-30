70796cf4df46dbb72fb999515cd277756eb7f218
learnforpractice <learnforpractice@gmail.com>
2018-07-31 05:33:06 +0800

    a working version of eosio.token in python

--- programs/pyeos/contracts/eosio_token/t.py
+++ programs/pyeos/contracts/eosio_token/t.py
@@ -15,30 +15,54 @@ from common import prepare, producer
 
 def init(func):
     def func_wrapper(*args, **kwargs):
-        prepare('mytoken', 'token.py', 'eosio.token.abi', __file__, 6)
+        prepare('eosio.token', 'token.py', 'eosio.token.abi', __file__, 6)
         func(*args, **kwargs)
     return func_wrapper
 
 @init
-def test_issue():
+def create():
     msg = {"issuer":"eosio","maximum_supply":"11000000000000.0000 EOS"}
-    r = eosapi.push_action('mytoken', 'create', msg, {'mytoken':'active'})
+    r = eosapi.push_action('eosio.token', 'create', msg, {'eosio.token':'active'})
+    print(r)
 
 @init
-def test_create():
-    r = eosapi.push_action('mytoken','issue',{"to":"eosio","quantity":"10000000000000.0000 EOS","memo":""},{'eosio':'active'})
+def issue():
+    r = eosapi.push_action('eosio.token','issue',{"to":"eosio","quantity":"10000000000.0000 EOS","memo":""},{'eosio':'active'})
     assert r
 
 @init
-def test_transfer():
-    msg = {"from":"eosio", "to":"hello", "quantity":"100.0000 EOS", "memo":"m"}
-    r = eosapi.push_action('mytoken', 'transfer', msg, {'eosio':'active'})
+def transfer():
+    msg = {"from":"eosio", "to":"hello", "quantity":"1.0000 EOS", "memo":"m"}
+    r = eosapi.push_action('eosio.token', 'transfer', msg, {'eosio':'active'})
 
 @init
 def test2(count=100):
     actions = []
     for i in range(count):
-        action = ['mytoken', 'sayhello', str(i), {'mytoken':'active'}]
+        action = ['eosio.token', 'sayhello', str(i), {'eosio.token':'active'}]
+        actions.append(action)
+
+    ret, cost = eosapi.push_actions(actions, True)
+    assert ret and not ret['except']
+    print('total cost time:%.3f s, cost per action: %.3f ms, actions per second: %.3f'%(cost/1e6, cost/count/1000, 1*1e6/(cost/count)))
+
+
+@init
+def test3(count=100):
+    actions = []
+    for i in range(count):
+        action = ['eosio.token','issue',{"to":"eosio","quantity":"1.0000 EOS","memo":str(i)},{'eosio':'active'}]
+        actions.append(action)
+
+    ret, cost = eosapi.push_actions(actions, True)
+    assert ret and not ret['except']
+    print('total cost time:%.3f s, cost per action: %.3f ms, actions per second: %.3f'%(cost/1e6, cost/count/1000, 1*1e6/(cost/count)))
+
+@init
+def test4(count=100):
+    actions = []
+    for i in range(count):
+        action = ['eosio.token','transfer',{"from":"eosio", "to":"hello", "quantity":"0.0010 EOS", "memo":str(i)},{'eosio':'active'}]
         actions.append(action)
 
     ret, cost = eosapi.push_actions(actions, True)
