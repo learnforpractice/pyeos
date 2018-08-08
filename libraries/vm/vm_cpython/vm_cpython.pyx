4fd728b7c19a06fda1ca73f1b0fafb16a274e090
learnforpractice <learnforpractice@gmail.com>
2018-08-08 15:30:58 +0800

    move debugging code to vm_cpython.pyx

--- libraries/vm/vm_cpython/vm_cpython.pyx
+++ libraries/vm/vm_cpython/vm_cpython.pyx
@@ -6,8 +6,10 @@ import sys
 import imp
 import logging
 import traceback
+import importlib
 
 import db
+import debug
 import eoslib
 
 cdef extern from "<stdint.h>":
@@ -42,7 +44,19 @@ cdef extern int cpython_setcode(uint64_t account, string& code):
         return 1
     return 0
 
+def debug_apply(receiver, account, action):
+    try:
+        _account = eoslib.n2s(account)
+        mod = importlib.import_module('{0}.{0}'.format(_account))
+        mod.apply(receiver, account, action)
+        return 1
+    except Exception as e:
+        logging.exception(e)
+    return 0
+
 cdef extern int cpython_apply(uint64_t receiver, uint64_t account, uint64_t action) with gil:
+    if debug.get_debug_contract() == receiver:
+        return debug_apply(receiver, account, action)
     try:
         if receiver in py_modules:
             module = py_modules[receiver]
@@ -74,3 +88,6 @@ cdef extern int cpython_call(uint64_t receiver, uint64_t func) with gil:
     except Exception as e:
         logging.exception(e)
     return 0
+
+
+
