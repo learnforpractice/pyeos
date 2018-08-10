31444ad139406a3c5406170ce38c5347188815d6
learnforpractice <learnforpractice@gmail.com>
2018-08-10 14:52:25 +0800

    migrate simpleauction example to eos

--- programs/pyeos/tests/simpleauction/simpleauction.py
+++ programs/pyeos/tests/simpleauction/simpleauction.py
@@ -1,9 +1,7 @@
 #porting from http://solidity.readthedocs.io/en/latest/solidity-by-example.html
-import ustruct as struct
+import struct
 from eoslib import *
-from backyard.solidity import *
-from backyard.storage import SDict, SList, storage_find, storage_get, storage_set, _get_hash, _pack, _unpack
-from backyard.token import transfer
+require = eosio_assert
 
 '''
     function SimpleAuction(
@@ -15,22 +13,148 @@ from backyard.token import transfer
     }
 '''
 
-code = N('auction1')
+_code = N('auction1')
 
-class SimpleAuction(object):
-    # The following is a so-called natspec comment,
-    # recognizable by the three slashes.
-    # It will be shown when the user is asked to
-    # confirm a transaction.
 
-    # Create a simple auction with `_biddingTime`
-    # seconds bidding time on behalf of the
-    # beneficiary address `_beneficiary`.
+def event(func):
+    def func_wrapper(self, *args):
+        print('TODO: event')
+        return func(self, *args)
+    return func_wrapper
+
+
+class SList(object):
+    def __init__(self, code, scope, table_id):
+        self.code = code
+        self.scope = scope
+        self.table_id = table_id
+        self.count = db.get_table_item_count(code, scope, table_id)
+        print('table count:', self.count)
+
+    def pack(self, obj):
+        if type(obj) is int:
+            return struct.pack('QB', obj, 0)
+        elif type(obj) is bytes:
+            return struct.pack('%dsB'%(len(obj),), obj, 1)
+        raise Exception('unsupported type', type(obj))
+    
+    def unpack(self, bs):
+        if bs[-1] == 0:
+            return struct.unpack('Q', bs[:8])[0]
+        elif bs[-1] == 1:
+            return bs[:-1]
+
+    def __setitem__(self, index, val):
+        if index < 0 or index >= self.count:
+            raise IndexError('list index out of range')
+        db.update_i64(index, self.code, self.pack(val))
+
+    def __getitem__(self, index):
+        if index < 0 or index >= self.count:
+            raise IndexError('list index out of range')
+        print('index', index)
+        itr = db.find_i64(self.code, self.scope, self.table_id, index)
+        if itr < 0:
+            raise Exception('index not found!')
+        print('index', index, 'it', itr)
+        value = db.get_i64(itr)
+        return self.unpack(value)
+
+    def append(self, value):
+        print('+++append:', value)
+        db.store_i64(self.scope, self.table_id, self.code, self.count, self.pack(value))
+        self.count += 1
+        print('+++append return:', self.count)
+
+    def __len__(self):
+        return self.count
+
+class SDict(object):
+    def __init__(self, code, scope, table_id):
+        self.code = code
+        self.scope = scope
+        self.table_id = table_id
+        self.count = db.get_table_item_count(code, scope, table_id)
+        print('table count:', self.count)
+
+    def pack(self, obj):
+        if type(obj) is int:
+            return struct.pack('QB', obj, 0)
+        elif type(obj) is bytes:
+            return struct.pack('%dsB'%(len(obj),), obj, 1)
+        raise Exception('unsupported type', type(obj))
+
+    def unpack(self, bs):
+        if bs[-1] == 0:
+            return struct.unpack('Q', bs[:8])[0]
+        elif bs[-1] == 1:
+            return bs[:-1]
+
+    def __setitem__(self, index, val):
+        itr = db.find_i64(self.code, self.scope, self.table_id, index)
+        if itr > 0:
+            db.update_i64(index, self.code, self.pack(val))
+        else:
+            db.store_i64(self.scope, self.table_id, self.code, index, self.pack(val))
+
+    def __getitem__(self, index):
+        itr = db.find_i64(self.code, self.scope, self.table_id, index)
+        if itr < 0:
+            raise Exception('index not found!')
+        print('index', index, 'it', itr)
+        value = db.get_i64(itr)
+        return self.unpack(value)
+
+    def __len__(self):
+        return self.count
+
+class Database(object):
+    def __init__(self, code, scope, table_id):
+        self.code = code
+        self.scope = scope
+        self.table_id = table_id
+
+    def store(self, payer=None):
+        if not payer:
+            payer = self.code
+        it = db.find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
+        if it >= 0:
+            db.update_i64(it, payer, self.pack())
+        else:
+            db.store_i64(self.scope, self.table_id, payer, self.get_primary_key(), self.pack())
+
+    def load(self):
+        it = db.find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
+        if it >= 0:
+            data = db.get_i64(it)
+            return self.unpack(data)
+        return False
+
+    def erase(self):
+        it = db.find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
+        if it >= 0:
+            db.remove_i64(it)
+
+    def get_primary_key(self):
+        raise Exception('should be implemented by subclass')
+
+    def pack(self):
+        raise Exception('should be implemented by subclass')
+
+    def unpack(self, data):
+        raise Exception('should be implemented by subclass')
+
+class Singleton(Database):
+    def __init__(self, code, scope, table_id):
+        super(Singleton, self).__init__(code, scope, table_id)
+        self.pk_value = table_id
+
+    def get_primary_key(self):
+        return self.pk_value
+
+class SimpleAuction(Singleton):
     def __init__(self):
-    # Parameters of the auction. Times are either
-    # absolute unix timestamps (seconds since 1970-01-01)
-    # or time periods in seconds.
-        self._table_id = N('auction')
+        super(SimpleAuction, self).__init__(_code, _code, _code)
         self.beneficiary = 0 #address(0)
         self.auctionEnd = 0
 
@@ -38,148 +162,24 @@ class SimpleAuction(object):
         self.highestBidder = 0
         self.highestBid = 0
 
-        # Allowed withdrawals of previous bids
-#        mapping(address => uint) pendingReturns;
-        self.pendingReturns = SDict(code, table_id=1)
         # Set to true at the end, disallows any change
         self.ended = 0
-        
-        self.load()
-
+        self.locked = 0
+        try:
+            self.load()
+        except:
+            pass
     def start(self):
-        _msg = read_action()
-        require(len(_msg) == 16)
-        self.beneficiary = int.from_bytes(_msg[8:], 'little')
-        self.auctionEnd = now() + int.from_bytes(_msg[:8], 'little')
-        self.save()
-
-    @property
-    def beneficiary(self):
-        id = _get_hash('beneficiary')
-        value = storage_get(code, self._table_id, id)
-        value = _unpack(value)
-        return value
-
-    @beneficiary.setter
-    def beneficiary(self, value):
-        self._beneficiary = value
-        id = _get_hash('beneficiary')
-        value = _pack(value)
-        storage_set(code, self._table_id, id, value)
-
-    @property
-    def auctionEnd(self):
-        id = _get_hash('auctionEnd')
-        value = storage_get(code, self._table_id, id)
-        value = _unpack(value)
-        return value
-
-    @auctionEnd.setter
-    def auctionEnd(self, value):
-        self._auctionEnd = value
-        id = _get_hash('auctionEnd')
-        value = _pack(value)
-        storage_set(code, self._table_id, id, value)
-
-    # Events that will be fired on changes.
-    @event 
-    def HighestBidIncreased(self, bidder: address, amount: int):
-        pass
-
-    @event 
-    def AuctionEnded(self, winner: address, amount: int):
-        pass
-
-    # Bid on the auction with the value sent
-    # together with this transaction.
-    # The value will only be refunded if the
-    # auction is not won.
-    @payable
-    def bid(self, sender, value):
-        # No arguments are necessary, all
-        # information is already part of
-        # the transaction. The keyword payable
-        # is required for the function to
-        # be able to receive Ether.
-
-        # Revert the call if the bidding
-        # period is over.
-        require(
-            now() <= self.auctionEnd,
-            "Auction already ended."
-        );
-
-        # If the bid is not higher, send the
-        # money back.
-        require(
-            value > self.highestBid,
-            "There already is a higher bid."
-        );
-
-        if self.highestBid != 0:
-            # Sending back the money by simply using
-            # highestBidder.send(highestBid) is a security risk
-            # because it could execute an untrusted contract.
-            # It is always safer to let the recipients
-            # withdraw their money themselves.
-            self.pendingReturns[self.highestBidder] += self.highestBid;
-
-        self.highestBidder = sender;
-        self.highestBid = value;
-        self.HighestBidIncreased(sender, value);
-
-    # Withdraw a bid that was overbid.
-    def withdraw(self, sender) -> bool:
-        '''
-        if not self.pendingReturns.find(sender):
-            return False
-        amount = self.pendingReturns[sender]
-        if amount < 0:
-            return False
-        # It is important to set this to zero because the recipient
-        # can call this function again as part of the receiving call
-        # before `send` returns.
-        self.pendingReturns[sender] = 0
-        '''
-        
-        auth = struct.pack('QQ', sender, N('active'))
-        t = transfer()
-        t._from = code
-        t.to = sender
-#        t.amount = amount
-        t.amount = 100
-        t.precision = 4
-        t.symbol = 'EOS'
-        t.memo = 'hello,world'
-        data = t.pack()
-        print(data)
-        t.unpack(data)
-        t.p()
-
-        require_auth(sender)
-        require_auth(code)
-        send_inline( N('eosio.token'), N('transfer'), auth, data)
-        #multi_index.hpp 695: cannot modify objects in table of another contract
-#        wasm_call(N('eosio.token'), 'apply', N('eosio.token'), N('eosio.token'), N('transfer'))
-
-        #send_inline is asynchronized, 
-        #it's imposible to check the result of send_inline
-        #if send_inline failed, 
-        #it will throw an exception,
-        #all the changes made in the transaction will be rewind
-        self.pendingReturns[sender] = amount
-        return True
-
-        '''
-        if not sender.send(amount):
-            # No need to call throw here, just reset the amount owing
-            self.pendingReturns[sender] = amount
-            return False;
-        '''
+        print('+++self.auctionEnd:', self.auctionEnd)
+        require(self.auctionEnd <=0, 'auction already started.')
+        msg = read_action()
+        self.beneficiary, self.auctionEnd = struct.unpack('QQ', msg)
+        require(self.auctionEnd > now())
+        self.store()
 
     # End the auction and send the highest bid
     # to the beneficiary.
-    def auctionEnd(self):
+    def end(self):
         # It is a good guideline to structure functions that interact
         # with other contracts (i.e. they call functions or send Ether)
         # into three phases:
@@ -199,58 +199,87 @@ class SimpleAuction(object):
 
         # 2. Effects
         self.ended = 1;
-        self.AuctionEnded(self.highestBidder, self.highestBid)
 
         # 3. Interaction
-        #FIXME
-        self.beneficiary.transfer(self.highestBid)
-        self.save()
+        transfer(self.code, self.beneficiary, self.highestBid)
+        self.store()
 
-    def load(self):
-        for key in self.__dict__:
-            if key[0] == '_' or key == 'pendingReturns':
-                continue
-            id = _get_hash(key)
-            value = storage_find(code, self._table_id, id)
-            if value:
-                value = _unpack(value)
-                setattr(self, key, value)
+    def reset(self):
+        require(not self.locked)
 
-    def save(self):
-        for key in self.__dict__:
-            if key[0] == '_' or key == 'pendingReturns':
-                continue
-            id = _get_hash(key)
-            value = self.__dict__[key]
-            value = _pack(value)
-            storage_set(code, self._table_id, id, value)
+        if self.highestBid:
+            transfer(self.code, self.highestBidder, self.highestBid)
+
+        self.beneficiary = 0
+        self.auctionEnd = 0
+        self.highestBidder = 0
+        self.highestBid = 0
+        self.ended = 0
+
+        self.store()
+
+    def bid(self, sender, value):
+        require(self.beneficiary, 'Auction not started yet')
+        require(now() <= self.auctionEnd, "Auction already ended.")
+
+        # If the bid is not higher, send the money back.
+        require(value > self.highestBid, "There already is a higher bid.")
+
+        if self.highestBid != 0:
+            transfer_inline(self.highestBidder, self.highestBid)
+
+        self.highestBidder = sender
+        self.highestBid = value
+        self.store()
+
+    def pack(self):
+        a1 = self.beneficiary
+        a2 = self.auctionEnd
+
+        # Current state of the auction.
+        a3 = self.highestBidder
+        a4 = self.highestBid
+        a5 = self.ended
+        a6 = self.locked
+
+        return struct.pack('QQQQBB', a1, a2, a3, a4, a5, a6)
+
+    def unpack(self, data):
+        a1, a2, a3, a4, a5, a6 = struct.unpack('QQQQBB', data)
+        self.beneficiary = a1
+        self.auctionEnd = a2
+
+        # Current state of the auction.
+        self.highestBidder = a3
+        self.highestBid = a4
+        self.ended = a5
+        self.locked = a6
+        return self
+
+EOS = 1397703940
 
 def apply(receiver, code, action):
-    if action == N('init'):
-        _msg = read_action()
-        require(len(_msg) == 16)
-        msg = Msg()
-        msg.sender = int.from_bytes(_msg[:8], 'little')
-        msg.value = int.from_bytes(_msg[8:], 'little')
-
-        auction = SimpleAuction()
-        auction.init()
-    elif action == N('start'):
+    if action == N('start'):
+        print('aution start:')
         auction = SimpleAuction()
         auction.start()
-    elif action == N('withdraw'):
-        _msg = read_action()
-#        require(len(_msg) == 8)
-        sender = int.from_bytes(_msg[8:16], 'little')
+    elif action == N('end'):
         auction = SimpleAuction()
-        auction.withdraw(sender)
+        auction.end()
+    elif action == N('reset'):
+        auction = SimpleAuction()
+        auction.reset()
     elif action == N('transfer'):
+        require(code == N('eosio.token'))
         msg = read_action()
+        print('transfer:', msg)
+        _from, to, amount, symbol = struct.unpack('QQQQ', msg[:32])
+        print('++++symbol:', symbol)
+        require(symbol == EOS)
+        require(to == _code)
         print('transfer', msg)
-        t = transfer()
-        t.unpack(msg)
-        t.p()
         auction = SimpleAuction()
-        auction.bid(t._from, t.amount)
+        auction.bid(_from, amount)
 
 
+pass
--- programs/pyeos/tests/simpleauction/t.py
+++ programs/pyeos/tests/simpleauction/t.py
@@ -1,5 +1,6 @@
 import os
 import time
+import struct
 
 import wallet
 import eosapi
@@ -9,83 +10,33 @@ from common import prepare, producer
 
 def init(func):
     def func_wrapper(*args, **kwargs):
-        prepare('auction1', 'simpleauction.py', 'simpleauction.abi', __file__)
+        prepare('auction1', 'simpleauction.py', 'simpleauction.abi', __file__, 6)
         return func(*args, **kwargs)
     return func_wrapper
 
 @init
-def start(beneficiary='hello', auctionEnd=10000):
-    with producer:
-        beneficiary = eosapi.N(beneficiary)
-        msg = int.to_bytes(beneficiary, 8, 'little')
-        msg += int.to_bytes(auctionEnd, 8, 'little')
-        r = eosapi.push_action('auction1','start', msg, {'auction1':'active'})
-        assert r
+def start(beneficiary='hello', auctionEnd=None):
+    if not auctionEnd:
+        auctionEnd = int(time.time()+60)
+    beneficiary = eosapi.N(beneficiary)
+    msg = struct.pack('QQ', beneficiary, auctionEnd)
+    r = eosapi.push_action('auction1','start', msg, {'auction1':'active'})
+    assert r
 
 @init
-def bid(price):
-    '''
-    with producer:
-        sender = eosapi.N('hello')
-        msg = int.to_bytes(sender, 8, 'little')
-        msg += int.to_bytes(price, 8, 'little')
-        r = eosapi.push_action('auction1','bid', msg, {'auction1':'active'})
-        assert r
-    '''
-    with producer:
-        msg = {"from":"hello", "to":"auction1", "quantity":"{0}.0000 EOS".format(price), "memo":"m"}
-        r = eosapi.push_action('eosio.token', 'transfer', msg, {'hello':'active', 'eosio.token':'active'})
-        assert r
+def end():
+    r = eosapi.push_action('auction1','end', '', {'auction1':'active'})
+    assert r
+
+#for test only
+@init
+def reset(): 
+    r = eosapi.push_action('auction1','reset', '', {'auction1':'active'})
+    assert r
 
 @init
-def withdraw():
-    '''
-    with producer:
-        sender = eosapi.N('hello')
-        msg = int.to_bytes(sender, 8, 'little')
-        r = eosapi.push_action('auction1', 'withdraw', msg, {'auction1':'active', 'hello':'active'})
-        assert r
-    '''
-#multi_index.hpp 695: cannot modify objects in table of another contract
-    with producer:
-        msg = {"from":"auction1", "to":"hello", "quantity":"1.0000 EOS", "memo":"m"}
-        r = eosapi.push_action('auction1', 'withdraw', msg, {'auction1':'active'})
-#        r = eosapi.push_action('eosio.token', 'transfer', msg, {'hello':'active', 'auction1':'active'})
-        assert r
-
-@init
-def test2(count):
-    import time
-    import json
-    actions = []
-    for i in range(count):
-        action = ['auction1', 'sayhello', str(i), {'auction1':'active'}]
-        actions.append(action)
-
-    ret, cost = eosapi.push_actions(actions, True)
-    assert ret
-    print('total cost time:%.3f s, cost per action: %.3f ms, actions per second: %.3f'%(cost/1e6, cost/count/1000, 1*1e6/(cost/count)))
-    eosapi.produce_block()
-
-@init
-def create():
-    with producer:
-        msg = {"issuer":"eosio","maximum_supply":"1000000000.0000 EOS","can_freeze":0,"can_recall":0, "can_whitelist":0}
-        r = eosapi.push_action('eosio.token', 'create', msg, {'eosio.token':'active'})
-        assert r
-
-@init
-def issue():
-    with producer:
-        r = eosapi.push_action('eosio.token','issue',{"to":"hello","quantity":"1000.0000 EOS","memo":""},{'eosio':'active'})
-        assert r
-
-@init
-def transfer():
-    with producer:
-        msg = {"from":"hello", "to":"auction1", "quantity":"100.0000 EOS", "memo":"m"}
-        r = eosapi.push_action('eosio.token', 'transfer', msg, {'hello':'active'})
-#        r = eosapi.push_action('eosio.token', 'transfer', msg, {'hello':'active', 'auction1':'active'})
-        assert r
-
+def bid(price=0.1):
+    msg = {"from":"eosio", "to":"auction1", "quantity":"%.4f EOS"%(price,), "memo":"m"}
+    r = eosapi.push_action('eosio.token', 'transfer', msg, {'eosio':'active', 'auction1':'active'})
+    assert r
 
