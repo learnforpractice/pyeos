be8b42886da7486674cc4dfd3ab9c489961f43fc
learnforpractice <learnforpractice@gmail.com>
2018-08-16 19:51:31 +0800

    improve inspector

--- libraries/vm/vm_cpython_ss/inspector.pyx
+++ libraries/vm/vm_cpython_ss/inspector.pyx
@@ -46,7 +46,8 @@ cdef extern int init_function_whitelist():
         whitelist_function(bltin)
         function_whitelist[bltin] = True
 
-__current_module = None
+current_module = None
+current_module_name = None
 
 def add_function_to_whitelist(func):
 #    print('+++++++++++=add_function_to_white_list:', func)
@@ -69,10 +70,21 @@ ModuleType = type(db)
 
 cdef extern int py_inspect_getattr(v, name):
 #    print('+++++++++py_inspect_getattr:',v, name)
-    if __current_module == v:
+    if current_module == v:
         return 1
     return filter_attr(v, name)
 
 def set_current_module(mod):
-    global __current_module
-    __current_module = mod
+    global current_module
+    global current_module_name
+    current_module = mod
+    current_module_name = current_module.__name__
+
+cdef extern int cy_is_class_in_current_account(obj):
+    global current_module_name
+    if not hasattr(obj, '__module__'):
+        return 0
+
+    if  current_module_name == obj.__module__:
+        return 1
+    return 0
