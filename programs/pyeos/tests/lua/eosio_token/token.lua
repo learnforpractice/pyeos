9a3515d2064374de936d47de597f31e1f0ca792d
learnforpractice <learnforpractice@gmail.com>
2018-09-01 15:41:45 +0800

    eosio.token for lua

--- programs/pyeos/tests/lua/eosio_token/token.lua
+++ programs/pyeos/tests/lua/eosio_token/token.lua
@@ -1,214 +1,115 @@
---[[
-import struct
+require 'string'
+require 'math'
 
-import db
-from eoslib import N
-import eoslib
+-- Lua classes
+function class(class_init, bases)
+    bases = bases or {}
+    local c = {}
+    for _, base in ipairs(bases) do
+        for k, v in pairs(base) do
+            c[k] = v
+        end
+    end
+    c._bases = bases
+    c = class_init(c)
+    local mt = getmetatable(c) or {}
+    mt.__call = function(_, ...)
+        local object = {}
+        setmetatable(object, {
+            __index = function(tbl, idx)
+                local method = c[idx]
+                if type(method) == "function" then
+                    return function(...)
+                        return c[idx](object, ...) 
+                    end
+                end
+                return method
+            end,
+        })
+        if type(object.__init__) == "function" then
+            object.__init__(...)
+        end
+        return object
+    end
+    setmetatable(c, mt)
+    return c
+end
 
-_code = N('eosio.token')
-#_code = N('mytoken')
+_code = N('tokentest')
 
-def to_symbol_name(s):
-    n = int.from_bytes(s, 'little')
-    return n>>8
-
-class Asset(object):
-    def __init__(self, amount=0, symbol=None):
-        self.amount = amount
-        self.symbol = symbol
-
-    def pack(self):
-        buffer = bytearray(16)
-        struct.pack_into('Q8s', buffer, 0, self.amount, self.symbol)
-        return bytes(buffer)
-
-    def unpack(cls, data):
-        a = Asset()
-        a.amount, a.symbol = struct.unpack('Q8s', data)
-        return a
-
-class multi_index(object):
-    def __init__(self, code, scope, table_id):
+local multi_index = class(function(multi_index)
+    function multi_index.__init__(self, code, scope, table_id)
         self.code = code
         self.scope = scope
         self.table_id = table_id
+    end
+    function multi_index.store(self, payer)
+      itr = db_find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
+      if itr >= 0 then
+        db_update_i64(itr, self.pack())
+      else
+        db_store_i64(self.scope, self.table_id, self.get_primary_key(), self.pack())
+      end
+      return 0;
+    end
 
-    def store(self, payer):
-        it = db.find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
-        if it >= 0:
-            db.update_i64(it, payer, self.pack())
-        else:
-            db.store_i64(self.scope, self.table_id, payer, self.get_primary_key(), self.pack())
+    function multi_index.load(self)
+      itr = db_find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
+      if itr < 0 then
+        return nil
+      end
+      data = db_get_i64(itr)
+      return self.unpack(data)
+    end
+    
+    function multi_index.erase(self)
+        itr = db_find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
+        if it >= 0 then
+            db_remove_i64(it)
+        end
+    end
 
-    def load(self):
-        it = db.find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
-        if it >= 0:
-            data = db.get_i64(it)
-            return self.unpack(data)
-        return False
+    function multi_index.get_primary_key(self)
+      error('get_primary_key not implemented by subclass')
+      return 0;
+    end
+    
+    function multi_index.pack(self)
+      error('pack not implemented by subclass')
+      return 0;
+    end
 
-    def erase(self):
-        it = db.find_i64(self.code, self.scope, self.table_id, self.get_primary_key())
-        if it >= 0:
-            db.remove_i64(it)
+    function multi_index.unapck(data)
+      error('unapck not implemented by subclass')
+      return 0;
+    end
 
-    def get_primary_key(self):
-        raise Exception('should be implemented by subclass')
-
-    def pack(self):
-        raise Exception('should be implemented by subclass')
-
-    def unpack(self, data):
-        raise Exception('should be implemented by subclass')
-
-class currency_stats(multi_index):
-    def __init__(self, symbol):
-        table_id = N('stat')
-        self.primary_key = scope = to_symbol_name(symbol)
-        multi_index.__init__(self, _code, scope, table_id)
-
-        self.supply = Asset(0, symbol)
-        self.max_supply =Asset(0, symbol)
-        self.issuer = 0
-
-        self.load()
-
-    def get_primary_key(self):
-        return self.primary_key
-
-    def pack(self):
-        result = bytearray(40)
-        s1 = self.supply
-        s2 = self.max_supply
-        struct.pack_into('Q8sQ8sQ', result, 0, s1.amount, s1.symbol, s2.amount, s2.symbol, self.issuer)
-        return bytes(result)
-
-    def unpack(self, data):
-        s1 = self.supply
-        s2 = self.max_supply
-        s1.amount, s1.symbol, s2.amount, s2.symbol, self.issuer = struct.unpack_from('Q8sQ8sQ', data)
-        return True
-
-class Singleton(multi_index):
-    def __init__(self, code, scope, table_id):
-        super(multi_index, self).__init__(code, scope, table_id)
-        self.pk_value = table_id
-
-    def get_primary_key(self):
-        return self.pk_value
-
-class Balance(multi_index):
-    def __init__(self, owner, symbol):
-        self.owner = owner
-        self.a = Asset(0, symbol)
-        self.symbol_name = to_symbol_name(symbol)
-
-        table_id = N('accounts')
-        multi_index.__init__(self, _code, self.owner, table_id)
-
-        self.load()
-
-    def add(self, amount, payer):
-        self.a.amount += amount
-        self.store(payer)
-
-    def sub(self, amount):
-        assert(self.a.amount >= amount)
-        self.a.amount -= amount
-        if self.a.amount == 0:
-            self.erase()
-        else:
-            self.store(self.owner)
-
-    def get_primary_key(self):
-        return self.symbol_name
-
-    def pack(self):
-        buffer = bytearray(16)
-        struct.pack_into('Q8s', buffer, 0, self.a.amount, self.a.symbol)
-        return bytes(buffer)
-
-    def unpack(self, data):
-        self.a.amount, self.a.symbol = struct.unpack('Q8s', data)
-
-def _create(msg):
-    eoslib.require_auth(_code)
-    issuer, amount, symbol = struct.unpack('QQ8s', msg)
-
-    cs = currency_stats(symbol)
-    if cs.issuer:
-        raise Exception('currency has already been created')
-    cs.issuer = issuer
-    cs.max_supply.amount = amount
-    cs.store(_code)
-
-def _issue(msg):
-    _to, amount, symbol = struct.unpack('QQ8s', msg[:24])
-#        memo = eoslib.unpack_bytes(msg[24:])
-    cs = currency_stats(symbol)
-    assert cs.issuer, 'currency does not exists'
-    eoslib.require_auth(cs.issuer)
-    cs.supply.amount += amount
-    assert cs.supply.amount < cs.max_supply.amount
-    cs.store(cs.issuer)
-
-    acc = Balance(_to, symbol)
-    acc.add(amount, cs.issuer)
-
-def _transfer(msg):
-    _from, to, amount, symbol = struct.unpack('QQQ8s', msg[:32])
-#    print('transfer:', _from, to, amount, symbol)
-    eoslib.require_auth(_from)
-    eoslib.require_recipient(_from)
-    eoslib.require_recipient(to)
-#        memo = eoslib.unpack_bytes(msg[32:])
-    a1 = Balance(_from, symbol)
-    a2 = Balance(to, symbol)
-    a1.sub(amount)
-    a2.add(amount, _from)
-
-def transfer():
-    msg = eoslib.call_get_args()
-    _transfer(msg)
+    return multi_index
+end, {})
 
 
-def apply(receiver, account, act):
-    if act == N('create'):
-        msg = eoslib.read_action()
-        _create(msg)
-    elif act == N('issue'):
-        msg = eoslib.read_action()
-        _issue(msg)
-    elif act == N('transfer'):
-        msg = eoslib.read_action()
-        _transfer(msg)
-]]--
-
+local B = class(function(B)
+    function B.__init__(self, a, b, c)
+        A.__init__(self, a, b)
+        self.c = c
+    end
+    function B.callme(self)
+        print("callme B")
+    end
+    return B
+end, {A})
 
 function to_symbol_name(s)
-    n = string.unpack('>i8', s)
-    return n>>8
+    n = string.unpack('n', s)
+    ret = rshift(n, 8)
+    print('+++:', n, ret)
+    return ret
 end
-
-class Asset(object):
-    def __init__(self, amount=0, symbol=None):
-        self.amount = amount
-        self.symbol = symbol
-
-    def pack(self):
-        buffer = bytearray(16)
-        struct.pack_into('Q8s', buffer, 0, self.amount, self.symbol)
-        return bytes(buffer)
-
-    def unpack(cls, data):
-        a = Asset()
-        a.amount, a.symbol = struct.unpack('Q8s', data)
-        return a
 
 Asset = {}
 Asset.__index = Asset
 --constructor
-function Asset.new(amount, symbol)
+function Asset:new(amount, symbol)
   local instance = {
     _amount = amount,
     _symbol = symbol
@@ -217,92 +118,122 @@ function Asset.new(amount, symbol)
   return instance
 end
 
-function Asset.pack()
-  string.format('%08x')
+function Asset:pack()
+  return string.pack('nn', self._amount, self._symbol)
 end
 
-function Asset.unpack(data)
-end
-
-multi_index = {}
-multi_index.__index = multi_index
---constructor
-function multi_index.new(code, scope, table_id)
-  local instance = {
-    _code = code,
-    _scope = scope,
-    _table_id = table_id
-  }
-  
-  setmetatable(instance, multi_index)
-  return instance
-end
-
-function multi_index:store(payer)
-  return 0;
-end
-
-function multi_index:load()
-  return 0;
-end
-
-function multi_index:erase()
-  return 0;
-end
-
-function multi_index:get_primary_key()
-  return 0;
-end
-
-function multi_index:pack()
-  return 0;
-end
-
-function multi_index:unapck(data)
-  return 0;
+function Asset:unpack(data)
+  self._amount, self._symbol = string.unpack(data)
 end
 
 
-Balance = {};
--- constructor
-Balance.__index = Balance
-function Balance.new(owner, symbol)
-  local instance = {
-    _owner = owner,
-    _symbol = symbol,
-    _symbol_name = to_symbol_name(symbol)
-  }
-  setmetatable(instance,Balance)
-  return instance
+local Asset = class(function(A)
+    function Asset.__init__(self, amount, symbol)
+        self.amount = amount
+        self.symbol = symbol
+    end
+
+    function Asset.pack(self)
+      return string.pack('nn', self.amount, self.symbol)
+    end
+    return Asset
+end, {})
+
+function Asset.unpack(self, data)
+    amount, symbol = string.unpack(data)
+    return Asset(amount, symbol)
 end
 
-setmetatable(Balance,{__index = multi_index})
+local Balance = class(function(Balance)
+    function Balance.__init__(self, owner, symbol)
+        self.owner = owner
+        self.a = Asset(0, symbol)
+        self.symbol_name = to_symbol_name(symbol)
 
+        table_id = N('accounts')
+        multi_index.__init__(self, _code, self.owner, table_id)
+        multi_index.load(self)
+    end
+    
+    function Balance.add(self, amount, payer)
+    end
+    
+    function Balance.sub(self, amount)
+    end
+    
+    function Balance.get_primary_key(self)
+        return self.symbol_name
+    end
+    
+    function Balance.pack(self)
+      return string.pack('nn', self.a.amount, self.a.symbol)
+    end
+    return Balance
+end, {multi_index})
 
-function Balance.add(amount, payer)
+function Balance.unpack(self, data)
+  owner, symbol = string.unpack('nn', data)
+  return Balance(owner,symbol)
 end
 
-function Balance.sub(amount)
-end
 
-function Balance.get_primary_key()
-end
+local currency_stats = class(function(currency_stats)
+    function currency_stats.__init__(self, symbol)
+        table_id = N('stat')
+        self.primary_key = to_symbol_name(symbol)
+        self.scope = self.primary_key
+        print(self.primary_key, self.scope)
 
-function Balance.pack()
-end
+        multi_index.__init__(self, _code, self.scope, table_id)
 
-function Balance.unpack(data)
-end
+        self.supply = Asset(0, symbol)
+        self.max_supply =Asset(0, symbol)
+        self.issuer = 0
+        self.load()
+    end
 
+    function currency_stats.get_primary_key(self)
+        return self.primary_key
+    end
+
+    function currency_stats.pack(self)
+        s1 = self._supply
+        s2 = self._max_supply
+        return string.pack('nc8nc8n', s1._amount, s1._symbol, s2._amount, s2._symbol, self._issuer)
+    end
+    
+    function currency_stats.unpack(self, data)
+        s1 = self.supply
+        s2 = self.max_supply
+        s1._amount, s1._symbol, s2._amount, s2._symbol, self._issuer = string.unpack('nc8nc8n', data)
+    end
+
+    return currency_stats
+end, {multi_index})
 
 function _create(msg)
+    require_auth(_code)
+    issuer, amount, symbol = string.unpack('JJJ', msg)
+    print(issuer, amount, symbol)
+    
+    cs = currency_stats(symbol)
+   
+    if cs.issuer then
+        error('currency has already been created')
+    end
+
+    cs._issuer = issuer
+    cs._max_supply.amount = amount
+    cs:store(_code)
 end
+
 
 function _issue(msg)
 end
 
 function _transfer(msg)
 end
+
 
 function apply(receiver, account, act)
   msg = read_action_data()
@@ -313,4 +244,5 @@ function apply(receiver, account, act)
   elseif act == N('transfer') then
     _transfer(msg)
   end
+  return 1
 end
