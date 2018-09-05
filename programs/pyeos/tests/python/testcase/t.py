9ddcd9262599c7d9edeba0697946f4bc6646f6be
learnforpractice <learnforpractice@gmail.com>
2018-09-05 09:43:50 +0800

    testing code for python vm

---
+++ programs/pyeos/tests/python/testcase/t.py
@@ -0,0 +1,23 @@
+import imp
+import marshal
+import importlib
+
+code = '''
+print('hello,worldddddddd')
+'''
+
+def t():
+    name = 'hello'
+    co = compile(code, name, 'exec')
+    r = marshal.dumps(co)
+    i = 0
+    for c in r:
+        i+=1
+        print('0x%02x,'%(c,), end='')
+        if i%16==0:
+            print()
+    print()
+    co = marshal.loads(r)
+    module = imp.new_module(name)
+    exec(co, module.__dict__)
+t()
