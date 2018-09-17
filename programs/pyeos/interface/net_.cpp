7a1203c3354457ab71e4ad11222fbcdc65799192
learnforpractice <learnforpractice@gmail.com>
2018-09-17 10:40:13 +0800

    fix sync problem when call net_plugin::disconnect

--- programs/pyeos/interface/net_.cpp
+++ programs/pyeos/interface/net_.cpp
@@ -6,6 +6,9 @@
 using namespace appbase;
 using namespace eosio;
 
+static std::shared_ptr<std::mutex> m(new std::mutex());
+static std::shared_ptr<std::condition_variable> cv(new std::condition_variable());
+
 net_plugin& get_net_plugin() {
    abstract_plugin& plugin = app().get_plugin<eosio::net_plugin>();
    return *static_cast<net_plugin*>(&plugin);
@@ -21,8 +24,18 @@ string connect_(const string& host) {
 }
 
 string disconnect_(const string& host) {
-   return get_net_plugin().disconnect( host );
+   std::shared_ptr<string> result = std::make_shared<string>();
+
+   appbase::app().get_io_service().post([result, host](){
+      *result = get_net_plugin().disconnect( host );
+      cv->notify_one();
+   });
+
+   std::unique_lock<std::mutex> lk(*m);
+   cv->wait(lk);
+   return *result;
 }
+
 PyObject* status_(const string& host) {
    auto s = get_net_plugin().status( host );
    return python::json::to_string(fc::variant(s));
